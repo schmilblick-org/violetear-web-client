101895248b9240bbf6bd042704193095f6409557
Leo Le Bouter <lle-bout@zaclys.net>
2019-06-15 20:35:34 +0200

    Implement Login/Register

--- src/main.rs
+++ src/main.rs
@@ -26,6 +26,7 @@ enum Scene {
     Loading,
     LoginRegister,
     FetchConfigError,
+    LoggedIn,
 }
 
 enum Msg {
@@ -33,7 +34,11 @@ enum Msg {
     FetchConfigDone(Result<Config, Error>),
     FetchConfigError,
     Login,
+    LoginDone(Result<LoginResponse, Error>),
+    LoginError,
     Register,
+    RegisterDone(Result<RegisterResponse, Error>),
+    RegisterError,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -50,6 +55,16 @@ struct State {
 struct LoginRegisterFormData {
     username: String,
     password: String,
+}
+
+#[derive(Deserialize)]
+struct LoginResponse {
+    token: Option<String>,
+}
+
+#[derive(Deserialize)]
+struct RegisterResponse {
+    token: Option<String>,
 }
 
 impl Component for Model {
@@ -84,8 +99,11 @@ impl Component for Model {
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
         match msg {
             Msg::FetchConfig => {
-                self.ft =
-                    Some(self.fetch_service.fetch(
+                if self.state.token.is_some() {
+                    self.scene = Scene::LoggedIn;
+                    true
+                } else {
+                    self.ft = Some(self.fetch_service.fetch(
                         Request::get("/config.json").body(Nothing).unwrap(),
                         self.link.send_back(
                             move |response: Response<Json<Result<Config, Error>>>| {
@@ -98,7 +116,8 @@ impl Component for Model {
                             },
                         ),
                     ));
-                false
+                    false
+                }
             }
             Msg::FetchConfigDone(response) => {
                 self.config = response.ok();
@@ -114,29 +133,84 @@ impl Component for Model {
                 true
             }
             Msg::Login => {
+                self.loginregister_error = None;
+
                 if let Some(config) = &self.config {
                     self.ft = Some(
                         self.fetch_service.fetch(
-                            Request::post(&format!("{}/login", config.api_url))
+                            Request::builder()
+                                .method("POST")
+                                .uri(&format!("{}/login", config.api_url))
+                                .header("Content-Type", "application/json")
                                 .body(Json(&self.loginregister_form))
                                 .unwrap(),
                             self.link.send_back(
-                                move |response: Response<Json<Result<Config, Error>>>| {
+                                move |response: Response<Json<Result<LoginResponse, Error>>>| {
                                     let (meta, Json(data)) = response.into_parts();
                                     if meta.status.is_success() {
-                                        Msg::FetchConfigDone(data)
+                                        Msg::LoginDone(data)
                                     } else {
-                                        Msg::FetchConfigError
+                                        Msg::LoginError
                                     }
                                 },
                             ),
                         ),
                     )
                 };
-
-                false
+                true
             }
-            Msg::Register => false,
+            Msg::LoginDone(response) => {
+                self.state.token = response
+                    .map(|login_response| login_response.token.unwrap())
+                    .ok();
+                self.storage_service.store(KEY, Json(&self.state));
+                self.scene = Scene::LoggedIn;
+                true
+            }
+            Msg::LoginError => {
+                self.loginregister_error = Some("Could not login".into());
+                true
+            }
+            Msg::Register => {
+                self.loginregister_error = None;
+
+                if let Some(config) = &self.config {
+                    self.ft = Some(
+                        self.fetch_service.fetch(
+                            Request::builder()
+                                .method("POST")
+                                .uri(&format!("{}/register", config.api_url))
+                                .header("Content-Type", "application/json")
+                                .body(Json(&self.loginregister_form))
+                                .unwrap(),
+                            self.link.send_back(
+                                move |response: Response<Json<Result<RegisterResponse, Error>>>| {
+                                    let (meta, Json(data)) = response.into_parts();
+                                    if meta.status.is_success() {
+                                        Msg::RegisterDone(data)
+                                    } else {
+                                        Msg::RegisterError
+                                    }
+                                },
+                            ),
+                        ),
+                    )
+                };
+                true
+            }
+            Msg::RegisterDone(response) => {
+                self.state.token = response
+                    .map(|register_response| register_response.token.unwrap())
+                    .ok();
+                self.storage_service.store(KEY, Json(&self.state));
+                self.scene = Scene::LoggedIn;
+                true
+            }
+            Msg::RegisterError => {
+                self.loginregister_error = Some("Could not register".into());
+                true
+            }
+            _ => false,
         }
     }
 }
@@ -163,7 +237,7 @@ impl Renderable<Model> for Model {
                             <button class="login-button", style="right: 20px", onclick=|_| Msg::Login,>
                                 { "Login" }
                             </button>
-
+                    </div>
                             <p>
                                 {
                                     if let Some(msg) = &self.loginregister_error {
@@ -173,7 +247,6 @@ impl Renderable<Model> for Model {
                                     }
                                 }
                             </p>
-                    </div>
                 </body>
             },
             Scene::FetchConfigError => html! {
@@ -181,6 +254,19 @@ impl Renderable<Model> for Model {
                     <h3 style="text-align: center;",>
                         { "Application configuration could not be loaded,
                             please reload the page to try again." }
+                    </h3>
+                </body>
+            },
+            Scene::LoggedIn => html! {
+                <body>
+                    <h3 style="text-align: center;",>
+                        {
+                            if let Some(token) = &self.state.token {
+                                format!("Logged in with token {}", token)
+                            } else {
+                                String::new()
+                            }
+                        }
                     </h3>
                 </body>
             },
